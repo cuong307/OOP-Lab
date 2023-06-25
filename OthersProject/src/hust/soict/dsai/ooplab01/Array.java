6ca1bc7f21f10c7fdb4e2af76132ad39259fdc19
cuong307 <131217248+cuong307@users.noreply.github.com>
2023-06-25 22:24:26 +0700

    commit lab03

---
+++ OthersProject/src/hust/soict/dsai/ooplab01/Array.java
@@ -0,0 +1,49 @@
+package hust.soict.dsai.ooplab01;
+
+import java.util.Scanner;
+
+public class Array {
+	public static void main(String[] args) {        
+        //Initialize array     
+    	int m;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input sizes of the array: ");
+		m = input.nextInt();
+	    int array[] = new int[m]; 
+	    System.out.print("Input elements of the array: ");
+	    for (int i = 0 ; i < m ; i++)
+	    	array[i] = input.nextInt();
+              
+            
+        //Sort the array in ascending order    
+        for (int i = 0; i < array.length; i++) {     
+        	for (int j = i+1; j < array.length; j++) {     
+        		if(array[i] > array[j]) {    
+        			int temp;
+        			temp = array[i];    
+        			array[i] = array[j];    
+        			array[j] = temp;    
+               }     
+            }     
+        }    
+          
+       
+        //Displaying elements of array after sorting    
+        System.out.println("Elements of array sorted in ascending order: ");    
+        for (int i = 0; i < array.length; i++) {     
+            System.out.print(array[i] + " ");    
+        }    
+        
+         
+        
+        //Calculate sum and the average of the array elements
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+        	sum += array[i];
+        }
+        double avg = (double)sum/(double)m;
+        System.out.println("Sum of the array elements: " + sum);
+        System.out.print("The average value of the array elements: " + avg);
+        
+    }  
+}
---
+++ OthersProject/src/hust/soict/dsai/ooplab01/Matrix.java
@@ -0,0 +1,42 @@
+package hust.soict.dsai.ooplab01;
+
+import java.util.Scanner;
+public class Matrix {
+	public static void main(String args[]) {
+		int m, n;
+		Scanner input = new Scanner(System.in);
+ 
+		System.out.print("Input number of rows of matrix: ");
+		m = input.nextInt();
+		System.out.print("Input number of columns of matrix: ");
+		n  = input.nextInt();
+ 
+	    int array1[][] = new int[m][n];
+	    int array2[][] = new int[m][n];
+	    int sum[][] = new int[m][n];
+ 
+	    System.out.println("Input elements of the first matrix: ");
+	    for (  int c = 0 ; c < m ; c++ )
+	    	for ( int d = 0 ; d < n ; d++ )
+	    		array1[c][d] = input.nextInt();
+ 
+	    System.out.println("Input the elements of the second matrix: "); 
+	    for ( int c = 0 ; c < m ; c++ )
+	    	for ( int d = 0 ; d < n ; d++ )
+	    		array2[c][d] = input.nextInt();
+ 
+	    // Add 2 matrices
+	    for (int  c = 0 ; c < m ; c++ )
+	    	for ( int d = 0 ; d < n ; d++ )
+	    		sum[c][d] = array1[c][d] + array2[c][d]; 
+ 
+	    System.out.println("Sum of the matrices: ");
+	    for ( int c = 0 ; c < m ; c++ ){
+	    	for ( int d = 0 ; d < n ; d++ )
+	    		System.out.print(sum[c][d]+"\t");
+ 
+         System.out.println();
+      }
+   }
+}  
+
