01ae3581c7adfc1606086e2034b3a0458ea86d4f
cuong307 <131217248+cuong307@users.noreply.github.com>
2023-06-25 23:45:37 +0700

    commit lab05

---
+++ GUIProject/src/hust/soict/dsai/javafx/PainterController.java
@@ -0,0 +1,20 @@
+package hust.soict.dsai.javafx;
+
+import java.awt.event.MouseEvent;
+
+public class PainterController {
+	
+	@FXML
+	private Pane drawingAreaPane;
+	
+	@FXML
+	void clearButtonPressed(ActionEvent event) {
+		drawingAreaPane.getChildren().clear;
+	}
+	
+	@FXML
+	void drawingAreaMouseDragged(MouseEvent event) {
+		Circle newCircle = new Circle(event.getX(),event.getY(), 4, Color.BLACK);
+		drawingAreaPane.getChildren().add(newCircle);
+	}
+}
---
+++ OthersProject/src/hust/soict/dsai/ooplab01/StarsTriangle.java
@@ -0,0 +1,25 @@
+package hust.soict.dsai.ooplab01;
+
+
+import java.util.Scanner;
+
+public class StarsTriangle {
+	public static void main(String []args) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("press interger n:");
+		int n = keyboard.nextInt();
+		//System.out.println("n = " + n);
+		
+		for (int i = 0; i<n; i++) {
+			for (int j = n; j>i; j--) {
+				System.out.print(" ");	
+			}		
+			for (int j = 0; (j <= 2*i); j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+		
+	}
+}
